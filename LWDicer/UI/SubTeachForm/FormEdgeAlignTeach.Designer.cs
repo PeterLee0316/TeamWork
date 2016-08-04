a23b2218f579e86d2a5518eaab1e1b7f87143747
designer96 <designer96@sfa.co.kr>
2016-08-04 15:35:55 +0900

    비전 Teaching  Form 제작

--- LWDicer/UI/SubTeachForm/FormEdgeAlignTeach.Designer.cs
+++ LWDicer/UI/SubTeachForm/FormEdgeAlignTeach.Designer.cs
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEdgeAlignTeach));
+            this.gradientLabel16 = new Syncfusion.Windows.Forms.Tools.GradientLabel();
+            this.gradientLabel1 = new Syncfusion.Windows.Forms.Tools.GradientLabel();
+            this.BtnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // gradientLabel16
+            // 
+            this.gradientLabel16.BackgroundColor = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.ForwardDiagonal, System.Drawing.Color.White, System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255))))));
+            this.gradientLabel16.BorderSides = ((System.Windows.Forms.Border3DSide)((((System.Windows.Forms.Border3DSide.Left | System.Windows.Forms.Border3DSide.Top) 
+            | System.Windows.Forms.Border3DSide.Right) 
+            | System.Windows.Forms.Border3DSide.Bottom)));
+            this.gradientLabel16.BorderStyle = System.Windows.Forms.Border3DStyle.Bump;
+            this.gradientLabel16.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gradientLabel16.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.gradientLabel16.Location = new System.Drawing.Point(-248, -177);
+            this.gradientLabel16.Name = "gradientLabel16";
+            this.gradientLabel16.Size = new System.Drawing.Size(594, 38);
+            this.gradientLabel16.TabIndex = 933;
+            this.gradientLabel16.Text = "Camera Teaching Table";
+            this.gradientLabel16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gradientLabel1
+            // 
+            this.gradientLabel1.BackgroundColor = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.ForwardDiagonal, System.Drawing.Color.White, System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255))))));
+            this.gradientLabel1.BorderSides = ((System.Windows.Forms.Border3DSide)((((System.Windows.Forms.Border3DSide.Left | System.Windows.Forms.Border3DSide.Top) 
+            | System.Windows.Forms.Border3DSide.Right) 
+            | System.Windows.Forms.Border3DSide.Bottom)));
+            this.gradientLabel1.BorderStyle = System.Windows.Forms.Border3DStyle.Bump;
+            this.gradientLabel1.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gradientLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.gradientLabel1.Location = new System.Drawing.Point(17, 13);
+            this.gradientLabel1.Name = "gradientLabel1";
+            this.gradientLabel1.Size = new System.Drawing.Size(594, 38);
+            this.gradientLabel1.TabIndex = 936;
+            this.gradientLabel1.Text = "Camera Teaching Table";
+            this.gradientLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BtnExit.Image = ((System.Drawing.Image)(resources.GetObject("BtnExit.Image")));
+            this.BtnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnExit.Location = new System.Drawing.Point(1120, 806);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(124, 61);
+            this.BtnExit.TabIndex = 935;
+            this.BtnExit.Text = " Exit";
+            this.BtnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnExit.UseVisualStyleBackColor = true;
             // 
             // FormEdgeAlignTeach
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(730, 500);
+            this.ClientSize = new System.Drawing.Size(1260, 880);
+            this.Controls.Add(this.gradientLabel1);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.gradientLabel16);
             this.Name = "FormEdgeAlignTeach";
             this.Text = "FormEdgeAlignTeach";
             this.ResumeLayout(false);
@@ -42,5 +95,9 @@
         }
 
         #endregion
+
+        private Syncfusion.Windows.Forms.Tools.GradientLabel gradientLabel16;
+        private Syncfusion.Windows.Forms.Tools.GradientLabel gradientLabel1;
+        private System.Windows.Forms.Button BtnExit;
     }
 }
--- LWDicer/UI/SubTeachForm/FormMacroAlignTeach.Designer.cs
+++ LWDicer/UI/SubTeachForm/FormMacroAlignTeach.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMacroAlignTeach));
+            this.gradientLabel1 = new Syncfusion.Windows.Forms.Tools.GradientLabel();
+            this.BtnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // gradientLabel1
+            // 
+            this.gradientLabel1.BackgroundColor = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.ForwardDiagonal, System.Drawing.Color.White, System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255))))));
+            this.gradientLabel1.BorderSides = ((System.Windows.Forms.Border3DSide)((((System.Windows.Forms.Border3DSide.Left | System.Windows.Forms.Border3DSide.Top) 
+            | System.Windows.Forms.Border3DSide.Right) 
+            | System.Windows.Forms.Border3DSide.Bottom)));
+            this.gradientLabel1.BorderStyle = System.Windows.Forms.Border3DStyle.Bump;
+            this.gradientLabel1.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gradientLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.gradientLabel1.Location = new System.Drawing.Point(17, 13);
+            this.gradientLabel1.Name = "gradientLabel1";
+            this.gradientLabel1.Size = new System.Drawing.Size(594, 38);
+            this.gradientLabel1.TabIndex = 938;
+            this.gradientLabel1.Text = "Camera Teaching Table";
+            this.gradientLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BtnExit.Image = ((System.Drawing.Image)(resources.GetObject("BtnExit.Image")));
+            this.BtnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnExit.Location = new System.Drawing.Point(1120, 806);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(124, 61);
+            this.BtnExit.TabIndex = 937;
+            this.BtnExit.Text = " Exit";
+            this.BtnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnExit.UseVisualStyleBackColor = true;
             // 
             // FormMacroAlignTeach
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(839, 499);
+            this.ClientSize = new System.Drawing.Size(1260, 880);
+            this.Controls.Add(this.gradientLabel1);
+            this.Controls.Add(this.BtnExit);
             this.Name = "FormMacroAlignTeach";
             this.Text = "FormMacroAlignTeach";
             this.ResumeLayout(false);
@@ -42,5 +77,8 @@
         }
 
         #endregion
+
+        private Syncfusion.Windows.Forms.Tools.GradientLabel gradientLabel1;
+        private System.Windows.Forms.Button BtnExit;
     }
 }
